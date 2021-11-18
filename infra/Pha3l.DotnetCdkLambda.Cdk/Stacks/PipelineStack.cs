214cfbf487cdf308a0e39d2a798083880e41cb50
Luke Merry <lmerry@gmail.com>
2021-11-18 00:35:52 -0700

    remove second region for now

--- infra/Pha3l.DotnetCdkLambda.Cdk/Stacks/PipelineStack.cs
+++ infra/Pha3l.DotnetCdkLambda.Cdk/Stacks/PipelineStack.cs
@@ -12,21 +12,8 @@ namespace Pha3l.DotnetCdkLambda.Cdk.Stacks
         {
             var pipeline = new CodePipeline(this, "Pipeline", new CodePipelineProps
             {
+                CrossAccountKeys = true,
                 PipelineName = "ApplicationPipeline",
-                // Synth = new ShellStep("Synth", new ShellStepProps
-                // {
-                //     Input = CodePipelineSource.GitHub("pha3l/Pha3l.DotnetCdkLambda", "main", new GitHubSourceOptions
-                //     {
-                //         Authentication = SecretValue.SecretsManager("GithubToken")
-                //     }),
-                //     Commands = new[]
-                //     {
-                //         "dotnet build",
-                //         "npx cdk synth"
-                //     },
-                
-                //     
-                // })
                 Synth = new CodeBuildStep("Synth", new CodeBuildStepProps
                 {
                     Input = CodePipelineSource.GitHub("pha3l/Pha3l.DotnetCdkLambda", "main", new GitHubSourceOptions
@@ -56,14 +43,14 @@ namespace Pha3l.DotnetCdkLambda.Cdk.Stacks
                 }
             }));
             
-            pipeline.AddStage(new AppStage(this, "Prod", new StageProps
-            {
-                Env = new Environment
-                {
-                    Account = "004969436191",
-                    Region = "us-east-1"
-                }
-            }));
+            // pipeline.AddStage(new AppStage(this, "Prod", new StageProps
+            // {
+            //     Env = new Environment
+            //     {
+            //         Account = "004969436191",
+            //         Region = "us-east-1"
+            //     }
+            // }));
         }
     }
 }
